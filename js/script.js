711700902c7a31bb1bb018bc969a2fdb9d1683f1
Andrei Yahorau <yahoraual92@gmail.com>
2022-03-27 11:42:10 +0300

    Implemented the following features:
    Preload;
    Searching for news by entering something into the input;
    Selection of the news by choosing a country;
    Replacing the image of some news that do not have their own image;

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const API_KEY = 'fbaed260130146c786b25f3b2ed50404';
 const choiceElem = document.querySelector('.js-choice');
 const newsList = document.querySelector('.news-list');
+const formSearch = document.querySelector('.form-search');
+const title = document.querySelector('.title');
 
 const choices = new Choices(choiceElem, {
     searchEnabled: false,
@@ -19,25 +21,61 @@ const getData = async (url) => {
     return data;
 };
 
+const getDateCorrectFormat = (isoDate) => {
+    const date = new Date(isoDate);
+    const fullDate = date.toLocaleString('en-GB', {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+    });
+
+    const fullTime = date.toLocaleString('en-GB', {
+        hour: 'numeric',
+        minute: 'numeric',
+    });
+
+    return `<span class="news-date">${fullDate}</span> ${fullTime}`;
+}
+
+const getImage = urlToImage => new Promise((resolve) => {
+    const image = new Image(270, 200);
+    image.addEventListener('load', () => {
+        resolve(image);
+    });
+
+    image.addEventListener('error', () => {
+        image.src = urlToImage || 'image/no-photo.jpg';
+        image.className = 'news-image';
+        resolve(image);
+    });
+
+    image.src = urlToImage || 'image/no-photo.jpg';
+    image.className = 'news-image';
+    return image;
+});
+
 const renderCard = (data) => {
     newsList.textContent = '';
 
-    data.forEach(news => {
+    data.forEach(async news => {
+        const {urlToImage, title, url, description, publishedAt, author} = news;
         const card = document.createElement('li');
         card.classList.add('news-item');
 
-        card.innerHTML = `
-            <img class="news-image" src="${news.urlToImage}" alt="${news.title}" width="270" height="200">
-            <h3 class="news-title">
-                <a href="${news.url}" class="news-link" target="_blank">${news.title}</a>
-            </h3>
-            <p class="news-description">${news.description}</p>
+        const image = await getImage(urlToImage);
+        image.alt = title;
+        card.append(image);
 
+        card.innerHTML += `
+            <h3 class="news-title">
+                <a href="${url}" class="news-link" target="_blank">${title || ''}</a>
+            </h3>
+            <p class="news-description">${description || ''}</p>
             <div class="news-footer">
-                <time class="news-datetime" datetime="${news.publishedAt}">
-                    <span class="news-date">${news.publishedAt}</span> 11:06
+                <time class="news-datetime" datetime="${publishedAt}">
+                    ${getDateCorrectFormat(publishedAt)}
                 </time>
-                <div class="news-author">${news.author}</div>
+                <div class="news-author">${author || ''}</div>
             </div>
         `;
 
@@ -47,8 +85,37 @@ const renderCard = (data) => {
 };
 
 const loadNews = async () => {
-    const data = await getData('https://newsapi.org/v2/top-headlines?country=it');
+    newsList.innerHTML = `<li class="preload"></li>`;
+    const country = localStorage.getItem('country') || 'ua';
+    choices.setChoiceByValue(country);
+    title.classList.add('hide');
+    const data = await getData(`https://newsapi.org/v2/top-headlines?country=${country}&pageSize=100`);
     renderCard(data.articles);
 };
+
+const loadSearch = async (value) => {
+    const data = await getData(`https://newsapi.org/v2/everything?q=${value}&pageSize=100`);
+    title.classList.remove('hide');
+    title.textContent = `According to your request “${value}” ${data.articles.length} news have been found`;
+    choices.setChoiceByValue('');
+    renderCard(data.articles);
+};
+
+choiceElem.addEventListener('change' , (event) => {
+    const value = event.detail.value;
+    localStorage.setItem('country', value);
+    loadNews(value);
+
+});
+
+formSearch.addEventListener('submit' , (event) => {
+    event.preventDefault();
+    const value = formSearch.search_input.value;
+    formSearch.reset();
+    loadSearch(value);
+    
+
+});
+
 
 loadNews();
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
-Figma template: https://www.figma.com/file/RoVKEmePmyt5Ef7YJCOHMw/NEWS-(Intensive)?node-id=0%3A1
+Figma template: 
+https://www.figma.com/file/RoVKEmePmyt5Ef7YJCOHMw/NEWS-(Intensive)?node-id=0%3A1
 
 Implemented API interaction, working with API key, for https://newsapi.org.
